b6f2f6b0706d7521c36fef53d37b4e4b30d13a86
Shabin <d.shabin@gmail.com>
2017-09-25 19:52:30 +0400

    infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,18 +1,68 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import InfiniteScroll from 'react-infinite-scroller';
 
 class App extends Component {
+
+  state ={data : [] , page : 1 ,  hasMoreItems : true}
+
+  async getData(page) {
+    console.log("get_data")
+   let req;
+   try {
+     req = await fetch("https://content.guardianapis.com/search?api-key=3811404c-4fff-44c6-b717-46c36d3e15ca&page=" + this.state.page)
+      const result_json = await req.json()
+      const ndata = result_json.response.results
+      //this.setState({data : data ,page : this.state.page+1})
+      const {data} = this.state;
+      // const items = this.state.items;
+      //data.push(ndata);
+      ndata.map(elem => {
+            data.push(elem)
+        })
+      this.setState({data , page : this.state.page+1 , hasMoreItems: true});
+      console.log(this.state.page)
+      console.log(this.state.data)
+   }catch (e){
+     alert(e.message)
+   }
+ }
+
+  componentDidMount(){
+    //this.getData()
+  }
+
+  onscrollfunc(){
+    console.log('yo')
+  }
+
   render() {
+    let data;
+    if(this.state.data[0]){
+      data = this.state.data.map(elem => {
+          return (
+            <p>{elem.id}</p>
+          );
+        })
+    }else{
+      data = ''
+    }
+
+
     return (
+
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <InfiniteScroll
+              pageStart={1}
+              loadMore={this.getData.bind(this)}
+              hasMore={this.state.hasMoreItems}
+              threshold = {200}
+              loader={<div className="loader">Loading ...</div>}>
+              <div>
+                {data}
+              </div>
+          </InfiniteScroll>
       </div>
     );
   }
