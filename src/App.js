1cb8d620b4be563f85543ba81063d3bc6fc0fbc5
Shabin <d.shabin@gmail.com>
2017-09-25 21:30:49 +0400

    Add readme

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import InfiniteScroll from 'react-infinite-scroller';
 
 class App extends Component {
 
-  state ={data : [] , page : 1 ,  hasMoreItems : true}
+  state ={data : [] , page : 1 ,  hasMoreItems : true ,}
 
   async getData(page) {
     console.log("get_data")
    let req;
    try {
-     req = await fetch("https://content.guardianapis.com/search?api-key=3811404c-4fff-44c6-b717-46c36d3e15ca&page=" + this.state.page)
+     req = await fetch("https://content.guardianapis.com/search?api-key=3811404c-4fff-44c6-b717-46c36d3e15ca&show-fields=thumbnail&page=" + this.state.page)
       const result_json = await req.json()
       const ndata = result_json.response.results
-      //this.setState({data : data ,page : this.state.page+1})
       const {data} = this.state;
-      // const items = this.state.items;
-      //data.push(ndata);
       ndata.map(elem => {
             data.push(elem)
-        })
+      })
       this.setState({data , page : this.state.page+1 , hasMoreItems: true});
       console.log(this.state.page)
       console.log(this.state.data)
@@ -29,29 +25,60 @@ class App extends Component {
    }
  }
 
+ async getDataForInterval() {
+   this.setState({is_30_second_pass : false})
+   console.log("get_data_for_interval")
+   let req;
+   try {
+     req = await fetch("https://content.guardianapis.com/search?api-key=3811404c-4fff-44c6-b717-46c36d3e15ca&show-fields=thumbnail&page=1")
+     const result_json = await req.json()
+     const tempdata = result_json.response.results
+     if (tempdata[0].id === this.state.data[0].id){
+       console.log("no need for update")
+     }else{
+       console.log("upadate needed")
+       const {data} = this.state;
+       tempdata.map(elem => {
+            data.push(elem)
+        })
+        this.setState({data})
+     }
+     console.log(this.state.page)
+     console.log(this.state.data)
+  }catch (e){
+    alert(e.message)
+  }
+}
+
   componentDidMount(){
-    //this.getData()
+    setInterval(function(){
+    }, 2000);
   }
 
-  onscrollfunc(){
-    console.log('yo')
-  }
 
   render() {
+    console.log(this.state)
     let data;
     if(this.state.data[0]){
+      console.log(this.state.data[0])
       data = this.state.data.map(elem => {
+        if (elem.fields){
           return (
-            <p>{elem.id}</p>
+            <div key={elem.id}>
+            <img src={elem.fields.thumbnail} alt="" width="100" height="100"/>
+            <p>{elem.sectionName}</p>
+            <p>{elem.webTitle}</p>
+            <a href={elem.webUrl} target="_blank">[Read more]</a>
+            </div>
           );
-        })
+        }
+      }
+    )
     }else{
       data = ''
     }
 
-
     return (
-
       <div className="App">
       <InfiniteScroll
               pageStart={1}
